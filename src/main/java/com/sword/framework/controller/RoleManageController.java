06205a9730ddb7b0764f440d742ea7b3ab6b64ba
szzuojian <szzuojian@gmail.com>
2019-01-24 13:36:01 +0800

    保存角色菜单

--- src/main/java/com/sword/framework/controller/RoleManageController.java
+++ src/main/java/com/sword/framework/controller/RoleManageController.java
@@ -1,7 +1,6 @@
 package com.sword.framework.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,12 +17,21 @@ public class RoleManageController {
 	public RoleManageService roleManageService;
 
 	@RequestMapping(value = "/role/addRole", method = RequestMethod.POST)
-	public @ResponseBody ReturnInfo addMenu(String roleName, String roleDesc) throws AuthenticationException {
+	public @ResponseBody ReturnInfo addMenu(String roleName, String roleDesc) {
 		String roleId = roleManageService.addRole(roleName, roleDesc);
 		ReturnInfo ret = new ReturnInfo();
 		ret.setSuccess(Constants.SUCCESS);
 		ret.setMessage("操作成功");
 		ret.setData("id", roleId);
+		return ret;
+	}
+
+	@RequestMapping(value = "/role/saveRoleMenus", method = RequestMethod.POST)
+	public @ResponseBody ReturnInfo saveRoleMenus(String roleId, String menus) {
+		roleManageService.saveRoleMenus(roleId, menus.split(","));
+		ReturnInfo ret = new ReturnInfo();
+		ret.setSuccess(Constants.SUCCESS);
+		ret.setMessage("操作成功");
 		return ret;
 	}
 
